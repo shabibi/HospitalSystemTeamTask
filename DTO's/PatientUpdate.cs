b3716e1f8afb4c38a36b861e8958d62cd325601e
Alanoud <Alanoud2022it@gmail.com>
2024-12-30 10:49:02 +0400

    fix updatePatient & test all function of patient also creat patientUpdateDTO

---
+++ DTO's/PatientUpdate.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace HospitalSystemTeamTask.DTO_s
+{
+    public class PatientUpdate
+    {
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+        ErrorMessage = "Password must be at least 8 characters long, including at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+        public string Password { get; set; }
+    }
+}
