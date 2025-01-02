3f7a0d6d28f85356f48a9f7253a7d97dcc989841
Mohammed <Mohammed.almanwari.01@gmail.com>
2025-01-02 11:20:26 +0400

    add ErrorMessage to BranchDTO

--- DTO's/BranchDTO.cs
+++ DTO's/BranchDTO.cs
@@ -1,8 +1,15 @@
-﻿namespace HospitalSystemTeamTask.DTO_s
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalSystemTeamTask.DTO_s
 {
     public class BranchDTO
     {
+        [Required(ErrorMessage = "Branch name is required.")]
+        [StringLength(100, ErrorMessage = "Branch name must be between 3 and 100 characters.", MinimumLength = 3)]
         public string BranchName { get; set; }
+
+        [Required(ErrorMessage = "Location is required.")]
+        [StringLength(100, ErrorMessage = "Location must be between 3 and 100 characters.", MinimumLength = 3)]
         public string Location { get; set; }
         public bool BranchStatus { get; set; }
 
