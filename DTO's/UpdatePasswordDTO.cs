44f02dd4753299498d0c13664cf1d48a49aa2f5f
Alanoud <Alanoud2022it@gmail.com>
2025-01-05 10:21:54 +0400

    add UpdatePassword

--- DTO's/UpdatePasswordDTO.cs
+++ DTO's/UpdatePasswordDTO.cs
@@ -1,8 +1,17 @@
-﻿namespace HospitalSystemTeamTask.DTO_s
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalSystemTeamTask.DTO_s
 {
     public class UpdatePasswordDTO
     {
+        [Required]
         public int UserId { get; set; }
+       // [Required]
+       // [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+       //ErrorMessage = "Password must be at least 8 characters long, including at least one uppercase letter, one lowercase letter, one number, and one special character.")]
         public string NewPassword { get; set; }
+      
+       
+        public string CurrentPassword { get; set; }
     }
 }
